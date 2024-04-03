cc2c64e2c21c6ba235a9c5fb677e831f2bd43566
SHRIYANSH KUMAR <142136591+S1h2r3i4@users.noreply.github.com>
2024-04-03 18:05:33 +0530

    Create number of 1 bits.cpp

---
+++ number of 1 bits.cpp
@@ -0,0 +1,278 @@
+Day 19 of hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#100DaysOfCode
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#100daysofcodechallenge
+🔴 Number of 1 bits
+
+ SOLUTION LINK:-
+https:
+I'm eager to hear your insights and perspectives! Drop your thoughts in the comments below. Let's spark some meaningful conversations. 💬✨
+.
+.
+.
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#codingjourney hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#problemsolving hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#techlife hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#leetcode hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#leetcodestreak hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#codejourney hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#coding hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#codingjourney hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#100daysofcode hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#100daysofcodechallenge hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#100daysofdsa hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#coderarmy hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag#programming hashtag
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+
+hashtag
+//////////////////////////solution///////////////////////////
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count=0;
+    for(int i=0;i<32;++i){
+        if((n>>i)&1){
+            ++count;
+        }    
+    }
+        return count;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

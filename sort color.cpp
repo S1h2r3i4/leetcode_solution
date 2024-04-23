13a86c502e152f4038138d628c5ba332adffaa31
SHRIYANSH KUMAR <142136591+S1h2r3i4@users.noreply.github.com>
2024-04-24 00:07:06 +0530

    Create sort color.cpp

---
+++ sort color.cpp
@@ -0,0 +1,22 @@
+/////////////////////////////Solution////////////////////
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       int low = 0; // Pointer for elements equal to 0 (red)
+       int high = nums.size() - 1; // Pointer for elements equal to 2 (blue)
+      int i = 0; // Iterator pointer
+    
+    while (i <= high) {
+        if (nums[i] == 0) {
+            std::swap(nums[i], nums[low]);
+            ++i;
+            ++low;
+        } else if (nums[i] == 1) {
+            ++i;
+        } else {
+            std::swap(nums[i], nums[high]);
+            --high;
+        }
+    }
+}        
+};

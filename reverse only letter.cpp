7c5f7fbdeebe0c3b3549b113d4b5c45022cc9cd4
SHRIYANSH KUMAR <142136591+S1h2r3i4@users.noreply.github.com>
2024-05-04 00:51:35 +0530

    Create reverse only letter.cpp

---
+++ reverse only letter.cpp
@@ -0,0 +1,21 @@
+<---------------------------// /////////////////////solution//////////////////////------------------------------>
+  class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+         int left = 0, right = s.size() - 1;
+
+    while (left < right) {
+        if (isalpha(s[left]) && std::isalpha(s[right])) {
+            swap(s[left], s[right]);
+            ++left;
+            --right;
+        } else if (!isalpha(s[left])) {
+            ++left;
+        } else if (!isalpha(s[right])) {
+            --right;
+        }
+    }
+
+    return s;
+}
+};

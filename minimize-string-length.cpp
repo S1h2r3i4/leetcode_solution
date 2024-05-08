10536874db3a7b6206d03c50922908f22e586284
SHRIYANSH KUMAR <142136591+S1h2r3i4@users.noreply.github.com>
2024-05-09 00:26:09 +0530

    Create minimize-string-length.cpp

---
+++ minimize-string-length.cpp
@@ -0,0 +1,15 @@
+<--------------------------------------------SOLUTION------------------------------------------------------------------------------------->
+  class Solution {
+public:
+    int minimizedStringLength(string s) {
+    vector<bool> v(26,0);
+        int ans = 0;
+        for(auto &i: s){
+            if(!v[i-'a']){
+                ans++;
+                v[i-'a'] = true;
+            }
+        }
+        return ans;
+    }
+};
